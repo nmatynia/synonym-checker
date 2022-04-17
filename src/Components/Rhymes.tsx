cca1c4d13d6d926babb22f080d26577c13d64d68
nmatynia <norbert.matyniaa@gmail.com>
2022-04-17 12:09:30 +0100

    Added class names to the elements of Rhymes component and updated it's styling.

--- src/Components/Rhymes.tsx
+++ src/Components/Rhymes.tsx
@@ -10,13 +10,16 @@ interface Props{
 const Rhymes:React.FC<Props> = ({word,wordData}) =>{
     return(
         <div className="RhymesContainer">
+            <h3>Words that rhyme with <span className="upperWord">{word.toUpperCase()}</span></h3>
+            <div className="rhymesInnerContainer">
             {wordData.rhymes.all.map((rhyme,rhymeIdx)=>{
                 return(
-                    <div key={rhymeIdx}>
+                    <div key={rhymeIdx}className="rhymeName">
                         {rhyme}
                     </div>
                 )
             })}
+            </div>
         </div>
     )
 }
