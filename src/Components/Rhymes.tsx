2d2d6a229fa985a73c4e747d9d738105bf3119e2
nmatynia <norbert.matyniaa@gmail.com>
2022-04-17 14:54:47 +0100

    Validated rhymes output

--- src/Components/Rhymes.tsx
+++ src/Components/Rhymes.tsx
@@ -8,17 +8,24 @@ interface Props{
 }
 
 const Rhymes:React.FC<Props> = ({word,wordData}) =>{
+    
+    const mappedRhymes = () =>{
+        return wordData.rhymes.all.map((rhyme,rhymeIdx)=>{
+            return(
+                <div key={rhymeIdx}className="rhymeName">
+                    {rhyme}
+                </div>
+            )
+        })
+    }
+
+    const noResults:JSX.Element = (<div className="noResults">Sorry, it seems like there is no rhymes for this word.</div>)
+
     return(
         <div className="RhymesContainer">
             <h3>Words that rhyme with <span className="upperWord">{word.toUpperCase()}</span></h3>
             <div className="rhymesInnerContainer">
-            {wordData.rhymes.all.map((rhyme,rhymeIdx)=>{
-                return(
-                    <div key={rhymeIdx}className="rhymeName">
-                        {rhyme}
-                    </div>
-                )
-            })}
+            {wordData.rhymes.hasOwnProperty('all')?mappedRhymes():noResults}
             </div>
         </div>
     )
