3558b91853f6e5dd49779d4e5876ccd2d5b2af9a
nmatynia <norbert.matyniaa@gmail.com>
2022-04-17 13:34:30 +0100

    Added logic to Meaning.tsx component and updated it's styling

--- src/Components/Meaning.tsx
+++ src/Components/Meaning.tsx
@@ -1,16 +1,28 @@
 import { render } from "@testing-library/react";
 import React from "react";
 import {IWordData} from '../interfaces';
+import '../Styles/Meaning.css'
 
 interface IProps{
     word: string;
     wordData: IWordData;
 }
 
-const Meaning: React.FC<IProps> = () =>{
+const Meaning: React.FC<IProps> = ({word,wordData}) =>{
+    const mappedData:JSX.Element[] = wordData.results.map((data,dataIdx)=>{
+        return(
+            <div key={dataIdx} className="definitionContainer">
+                <div className="partOfSpeech">{data.partOfSpeech}</div>
+               <div className="definition">{data.definition}</div>
+               {data.examples ? (<div className="example">"{data.examples}"</div>):null}
+            </div>
+        )
+    })
+
     return(
-        <div>
-            
+        <div className="MeaningContainer">
+            <h3 className ="meaningHeader">{word} <span className ="pronunciation">/{wordData.pronunciation.all}/</span></h3>
+            {mappedData}
         </div>
     )
 }
